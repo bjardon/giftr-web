f6fcdf1d89841aba0a0bc836f18bed1c7e9f9e20
Bruno Jardon <bjardon97@gmail.com>
2024-11-24 20:19:55 -0600

    config: add auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import {
+    AuthGuard,
+    redirectLoggedInTo,
+    redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
+import { SignInPage, SignUpPage } from './auth/pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        data: { authGuardPipe: () => redirectLoggedInTo('/hub') },
+        canActivate: [AuthGuard],
+        // canActivateChild: [AuthGuard],
+        children: [
+            { path: '', pathMatch: 'full', redirectTo: 'sign-in' },
+            { path: 'sign-in', component: SignInPage },
+            { path: 'sign-up', component: SignUpPage },
+        ],
+    },
+];
